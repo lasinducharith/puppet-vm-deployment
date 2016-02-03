0264cd54005a1866c4f1aecfcae4fa889f80782e
lasinducharith <lasinducharith@gmail.com>
2016-02-03 12:11:26 +0530

    Initial files

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # puppet-vm-deployment
+
+A single shell script to deploy wso2 products in the cloud via puppet
+
+## What does it do?
+
+Once you prepare the puppet modules and hieradata, this script will ..
+
+ - Install required bundles (puppet/unzip) in the remote instance
+ - Install dependent puppet modules (stdlib/java)
+ - copy puppet modules and hieradata from local machine to remote server
+ - export facters
+ - Run puppet (puppet apply): does not use a puppet master
+
+## How to use
+
+1. Clone puppet-vm-deployment repository
+
+   ````
+   git clone https://github.com/lasinducharith/puppet-vm-deployment.git
+   ````
+
+2. Clone WSO2 puppet modules repository and consider this path in local machine as puppet_home:
+
+   ````
+   git clone https://github.com/wso2/puppet-modules.git
+   ````
+
+3. Download and copy Oracle JDK 1.7 distribution to the following path:
+
+    ````
+    [puppet_home]/modules/wso2base/files/jdk-7u80-linux-x64.tar.gz
+    ````
+
+4. Download and copy required product distributions, patches, dropins etc. to each puppet module
+    
+    ````
+    [puppet_home]/modules/wso2esb/files
+    [puppet_home]/modules/wso2am/files
+    [puppet_home]/modules/wso2as/files
+    ````
+
+5. Update puppet_home/hieradata with hostnames, ipaddresses and other configuration information
+    
+    ````
+    [puppet_home]/hieradata/dev/wso2/wso2esb/4.9.0
+    [puppet_home]/hieradata/dev/wso2/wso2am/1.9.0
+    ````
+
+6. Update server configuration details in server_configs.txt. You can include details of each server instance, starting from a new line
+    
+    ````
+    #instance_ip,product_name,product_version,product_profile,environment
+    192.168.100.1,wso2esb,4.9.0,manager,dev
+    192.168.100.2,wso2esb,4.9.0,worker,dev
+    ````
+
+7. Update puppet_home, key_file, destination_path, user in deploy_servers.sh
+
+8. Execute deploy_servers.sh
---
+++ deploy_servers.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+puppet_home="/home/lasindu/wso2/puppet-modules"
+key_file="/home/lasindu/wso2/key-file"
+destination_path="/home/ubuntu"
+user="ubuntu"
+
+function print_message () {
+  echo "----------------------------------------------------"
+  echo $1
+  echo "----------------------------------------------------"
+}
+
+while read server_config; do
+  IFS=',' read -r -a array <<< $server_config
+  print_message "Server Configurations"
+  echo "IP: ${array[0]}"
+  echo "Product Name: ${array[1]}"
+  echo "Product Version: ${array[2]}"
+  echo "Product Profile: ${array[3]}"
+  echo "Environment: ${array[4]}"
+
+  print_message "Copying Puppet modules to Server"
+  scp -o "StrictHostKeyChecking=no" -i $key_file -r $puppet_home $user@${array[0]}:$destination_path
+  
+  ssh -o "StrictHostKeyChecking=no" -i $key_file $user@${array[0]} << EOF
+  sudo -i
+  echo "----------------- Installing puppet, unzip and tree -----------------"
+  apt-get update
+  apt-get -y install puppet unzip tree
+  echo "----------------- Installing dependent puppet modules --------------------"
+  puppet module install puppetlabs/stdlib --version 4.11.0
+  puppet module install 7terminals-java --version 0.0.8
+  echo "----------------- Copying files to /etc/puppet -------------------"
+  cp -r $destination_path/puppet-modules/modules/* /etc/puppet/modules/
+  cp -r $destination_path/puppet-modules/hieradata/ /etc/puppet/
+  cp $destination_path/puppet-modules/hiera.yaml /etc/puppet/
+  cp $destination_path/puppet-modules/manifests/site.pp /etc/puppet/manifests/
+  echo "----------------- Exporting Facters --------------------"
+  export FACTER_product_name=${array[1]}
+  export FACTER_product_version=${array[2]}
+  export FACTER_product_profile=${array[3]}
+  export FACTER_environment=${array[4]}
+
+  cd /etc/puppet
+  echo "----------------- Run puppet apply --------------------------"
+  puppet apply --modulepath=/etc/puppet/modules/ -e "include ${array[1]}" -v --detailed-exitcodes
+
+EOF
+done < server_configs.txt
